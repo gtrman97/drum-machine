fae3dc9658ae40cf71c28491a0f4e8a6908420bf
gtrman97 <andrew.killian@g.austincc.edu>
2021-09-08 20:26:37 -0500

    added audio time

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener('keydown', function(event) {
     const audio = this.document.querySelector(`audio[data-key="${event.keyCode}"]`);
     if(!audio) return; 
+    audio.currentTime = 0; // lets you play same drum sound fast in succession 
     audio.play(); 
 }); 

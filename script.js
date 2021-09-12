9f09800713b2d6bceb12fa68a5ee10a1f130c2b6
gtrman97 <andrew.killian@g.austincc.edu>
2021-09-12 01:50:50 -0500

    added code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-window.addEventListener('keydown', function(event) {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.drum[data-key="${e.keyCode}"]`);
     if(!audio) return; // if button pressed has no matching data-key, do nothing
     audio.currentTime = 0; // lets you play same drum sound fast in succession 
     audio.play(); 

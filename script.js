901c2725fcc4933e1c19de6301792e902418b718
gtrman97 <andrew.killian@g.austincc.edu>
2021-09-12 01:59:03 -0500

    added code

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ window.addEventListener('keydown', function(e) {
     if(!audio) return; // if button pressed has no matching data-key, do nothing
     audio.currentTime = 0; // lets you play same drum sound fast in succession 
     audio.play(); 
+    key.classList.add('playing'); 
 }); 

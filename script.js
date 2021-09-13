3f27688889fe262245e54789f7a87b5c7c2e218b
gtrman97 <andrew.killian@g.austincc.edu>
2021-09-13 02:19:09 -0500

    changed variable name

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.drum[data-key="${e.keyCode}"]`);
+    const drum = document.querySelector(`.drum[data-key="${e.keyCode}"]`);
     if(!audio) return; // if button pressed has no matching data-key, do nothing
     audio.currentTime = 0; // lets you play same drum sound fast in succession 
     audio.play(); 
-    key.classList.add('playing'); 
+    drum.classList.add('playing'); 
 }); 
